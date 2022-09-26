b7832c502c83760ae05cd9728afd4daeed7578b2
Jesper Engberg <jeengbe@gmail.com>
2022-09-26 21:07:21 +0200

    Normalize Guard/Valit parameters

---
+++ src/index.ts
@@ -0,0 +1,70 @@
+import { NodePath, PluginObj } from "@babel/core";
+import * as t from "@babel/types";
+
+export default (): PluginObj => ({
+  name: "vality",
+  visitor: {
+    MemberExpression: visit,
+    CallExpression: visit,
+  },
+});
+
+function visit(path: NodePath<t.Node>) {
+  switch (path.type) {
+    case "MemberExpression":
+      return visitMemberExpression(path as NodePath<t.MemberExpression>);
+    case "CallExpression":
+      return visitCallExpression(path as NodePath<t.CallExpression>);
+  }
+}
+
+function visitMemberExpression(path: NodePath<t.MemberExpression>) {
+  const { node } = path;
+  if (isGuard(node)) {
+    path.replaceWith(t.callExpression(node, []));
+  } else if (isValit(node)) {
+    path.replaceWith(t.callExpression(t.callExpression(node, []), []));
+  }
+
+  return path.skip();
+}
+
+function visitCallExpression(path: NodePath<t.CallExpression>) {
+  const {
+    node: { callee, arguments: args },
+  } = path;
+
+  if (isGuard(callee)) return path.skip();
+
+  if (t.isCallExpression(callee)) {
+    if (!isValit(callee.callee)) return path.skip();
+
+    (path.get("callee").get("arguments") as NodePath<t.Node>[]).forEach((arg) =>
+      visit(arg)
+    );
+    return path.skip();
+  }
+
+  path.replaceWith(t.callExpression(path.node, []));
+  return path.skip();
+}
+
+function isValit(node: t.Node): boolean {
+  return (
+    t.isMemberExpression(node) &&
+    t.isIdentifier(node.object) &&
+    (node.object.name === "v" || node.object.name === "vality") &&
+    t.isIdentifier(node.property) &&
+    node.property.name === "array" // Here is where we would check for the type
+  );
+}
+
+function isGuard(node: t.Node): boolean {
+  return (
+    t.isMemberExpression(node) &&
+    t.isIdentifier(node.object) &&
+    (node.object.name === "v" || node.object.name === "vality") &&
+    t.isIdentifier(node.property) &&
+    node.property.name === "number" // Here is where we would check for the type
+  );
+}
